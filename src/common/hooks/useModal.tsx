abbe3a21b7e5c33c96c540263dc131996848f496
Takhyun Kim <kimkih1218@gmail.com>
2023-03-05 11:25:54 +0900

    feat: add common useModal hook

---
+++ src/common/hooks/useModal.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export function useModal() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return { isOpen, openModal, closeModal };
+}
