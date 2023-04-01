e523b96affdd10af9cf11a93556dde38b6eeff23
Takhyun Kim <kimkih1218@gmail.com>
2023-04-01 22:54:08 +0900

    feat: add Layout Component used by Mui Components

---
+++ src/common/components/Layout/Layout.tsx
@@ -0,0 +1,40 @@
+import Box from '@mui/material/Box';
+
+import { Header } from '@/common/components/Header';
+
+import type { ReactNode } from 'react';
+
+type LayoutProps = {
+  children?: ReactNode;
+  onUserProfileClick: () => void;
+};
+
+export default function Layout({ children, onUserProfileClick }: LayoutProps) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minWidth: '100%',
+        minHeight: '100vh',
+        backgroundColor: '#efefef',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'relative',
+          width: '73%',
+          height: '95vh',
+          padding: '24px',
+          paddingTop: 0,
+          borderRadius: '24px',
+          backgroundColor: '#ffffff',
+        }}
+      >
+        <Header onUserProfileClick={onUserProfileClick} />
+        {children}
+      </Box>
+    </Box>
+  );
+}
