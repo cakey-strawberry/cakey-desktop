7046a7773ca7f88c905a13b27816d1d59ae0e5f1
Takhyun Kim <kimkih1218@gmail.com>
2023-04-01 22:52:16 +0900

    chore: add jsdoc to deprecated Modal component

--- src/common/components/Modal.tsx
+++ src/common/components/Modal.tsx
@@ -4,15 +4,17 @@ type ModalProps = {
   children?: React.ReactNode;
   buttonText: string;
   onClick: () => void;
-  some?: () => void;
 };
 
-export default function Modal({
-  children,
-  buttonText,
-  onClick,
-  some,
-}: ModalProps) {
+/**
+ *
+ * 이 컴포넌트는 사용하지 않습니다.
+ * 초기 개발 단계에서 사용했던 컴포넌트입니다.
+ * @deprecated
+ * @param {React.ReactNode} children
+ * @return {React.ReactElement}
+ */
+export default function Modal({ children, buttonText, onClick }: ModalProps) {
   return (
     <div className={styles.modal}>
       <div className={styles.modal_contents}>
