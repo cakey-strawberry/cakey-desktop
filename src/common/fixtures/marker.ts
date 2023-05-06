c025d452db3b686e8b3e0df0a7704965d09f33ec
Takhyun Kim <kimkih1218@gmail.com>
2023-05-07 00:28:22 +0900

    chore: modify Marker mock type and value

--- src/common/fixtures/marker.ts
+++ src/common/fixtures/marker.ts
@@ -1,28 +1,33 @@
 export type MarkerInfo = {
   id: number;
   position: google.maps.LatLngLiteral;
-  status: 'default' | 'select' | 'bookmark';
+  type: 'default' | 'bookmark';
+  selected: boolean;
 };
 
 export const MOCK_MARKERS: MarkerInfo[] = [
   {
     id: 1,
     position: { lat: 37.499245778231405, lng: 127.02520501188661 },
-    status: 'bookmark',
+    type: 'bookmark',
+    selected: true,
   },
   {
     id: 2,
     position: { lat: 37.49690498447068, lng: 127.02651392988588 },
-    status: 'select',
+    type: 'bookmark',
+    selected: false,
   },
   {
     id: 3,
     position: { lat: 37.497798750747975, lng: 127.03025829367067 },
-    status: 'default',
+    type: 'default',
+    selected: false,
   },
   {
     id: 4,
     position: { lat: 37.49774767867745, lng: 127.03054797224428 },
-    status: 'select',
+    type: 'bookmark',
+    selected: false,
   },
 ];
