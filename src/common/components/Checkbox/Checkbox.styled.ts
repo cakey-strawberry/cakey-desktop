1f111639874111f574edd72581a7bbde390fd794
Self-knowledge <qmfahs@gmail.com>
2023-05-31 07:35:32 +0900

    feat: add common Checkbox component

---
+++ src/common/components/Checkbox/Checkbox.styled.ts
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material/styles';
+import Checkbox from '@mui/material/Checkbox';
+
+const CustomCheckbox = styled(Checkbox)({
+  color: '#495057',
+  borderRadius: '2px',
+
+  // NOTE: Checkbox에서 V 표시(체크 표시)를 제외한 나머지를 채우고 있는 배경색
+  '&.Mui-checked': {
+    color: '#FF5775',
+  },
+});
+
+export default CustomCheckbox;
