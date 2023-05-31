38f0843b0e35aa575cf6232b4558e226538686ad
Self-knowledge <qmfahs@gmail.com>
2023-06-01 02:08:10 +0900

    feat: add layout for congrat-sign-up page component

---
+++ src/pages/congrat-sign-up.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/navigation';
+
+import { Congratulation } from '@/common/components/Congratulation';
+
+export default function CongratSignUp() {
+  const router = useRouter();
+
+  function handleButtonClick() {
+    router.push('/');
+  }
+
+  return (
+    <Congratulation
+      title="진심으로 환영합니다!"
+      subtitle="가입이 완료 되었어요."
+      buttonText="로그인하기"
+      onButtonClick={handleButtonClick}
+    />
+  );
+}
