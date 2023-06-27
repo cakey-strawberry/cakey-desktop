4d05612bbc599c5764e40830fed4bb7f90df65ed
Takhyun Kim <kimkih1218@gmail.com>
2023-06-27 21:13:59 +0900

    feat: add ReviewWriteModal component

---
+++ src/components/Main/ReviewWriteModal/ReviewWriteModal.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+
+import { Button } from '@/common/components/Button';
+import { Dialog } from '@/common/components/Dialog';
+
+import TagList from './TagList';
+import StoreInfo from './StoreInfo';
+import AddingPhoto from './AddingPhoto';
+import ReviewComment from './ReviewComment';
+
+import type { ChangeEvent } from 'react';
+
+type ReviewWriteModalProps = {
+  isOpen: boolean;
+  onCloseButtonClick: () => void;
+};
+
+export default function ReviewWriteModal({
+  isOpen,
+  onCloseButtonClick,
+}: ReviewWriteModalProps) {
+  const [comment, setComment] = useState<string>('');
+
+  function handleReviewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setComment(event.target.value);
+  }
+
+  return (
+    <Dialog.Wrapper
+      width="456px"
+      headerText="리뷰쓰기"
+      open={isOpen}
+      onClose={onCloseButtonClick}
+    >
+      <Dialog.Content
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignContent: 'center',
+        }}
+      >
+        <StoreInfo />
+        <ReviewComment
+          value={comment}
+          onTextFieldChange={handleReviewCommentChange}
+        />
+        <AddingPhoto />
+        <TagList />
+      </Dialog.Content>
+      <Dialog.Actions
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <Button
+          sx={{
+            width: '100%',
+            height: '56px',
+            fontSize: '16px',
+            color: '#b4b9bc',
+            backgroundColor: '#e9ecef',
+            borderRadius: '100px',
+          }}
+        >
+          리뷰 쓰기 완료
+        </Button>
+      </Dialog.Actions>
+    </Dialog.Wrapper>
+  );
+}
---
+++ src/components/Main/ReviewWriteModal/index.ts
@@ -0,0 +1,1 @@
+export { default as ReviewWriteModal } from './ReviewWriteModal';
