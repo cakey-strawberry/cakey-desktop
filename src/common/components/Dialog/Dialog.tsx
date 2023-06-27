dac628f456d535cd10fe4cfefbdaff8367942aaa
Takhyun Kim <kimkih1218@gmail.com>
2023-06-27 21:12:05 +0900

    style: modify common Dialog header layout

--- src/common/components/Dialog/Dialog.tsx
+++ src/common/components/Dialog/Dialog.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 import CloseIcon from '@/common/assets/icons/close.svg';
 
@@ -16,6 +17,7 @@ import type { DialogProps } from '@mui/material/Dialog';
 type DialogWrapperProps = DialogProps & {
   // this is the only way to override the width of the dialog
   width?: string;
+  headerText?: string;
 };
 
 function DialogWrapper(props: DialogWrapperProps) {
@@ -33,13 +35,28 @@ function DialogWrapper(props: DialogWrapperProps) {
       <Box
         sx={{
           width: '100%',
+          height: '24px',
           display: 'flex',
-          justifyContent: 'flex-end',
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'relative',
+          marginBottom: '16px',
         }}
       >
+        {props.headerText && (
+          <Typography sx={{ fontWeight: 'bold', fontSize: '22px' }}>
+            {props.headerText}
+          </Typography>
+        )}
         <Button
           onClick={handleClose}
-          sx={{ minWidth: 0, width: '24px', height: '24px' }}
+          sx={{
+            minWidth: 0,
+            width: '24px',
+            height: '24px',
+            position: 'absolute',
+            right: 0,
+          }}
         >
           <Image src={CloseIcon} alt="close" />
         </Button>
