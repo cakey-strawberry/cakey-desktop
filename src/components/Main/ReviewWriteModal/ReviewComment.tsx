142cc12816228bb81276207a2b6fb0b92339902e
Takhyun Kim <kimkih1218@gmail.com>
2023-06-27 21:13:41 +0900

    feat: add ReviewComment component layout

---
+++ src/components/Main/ReviewWriteModal/ReviewComment.tsx
@@ -0,0 +1,56 @@
+import { styled } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+import type { ChangeEvent } from 'react';
+
+type ReviewCommentProps = {
+  value: string;
+  onTextFieldChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+export default function ReviewComment({
+  value,
+  onTextFieldChange,
+}: ReviewCommentProps) {
+  return (
+    <ReviewCommentWrapper>
+      <Typography
+        sx={{
+          fontSize: '16px',
+          fontWeight: 700,
+          color: '#111111',
+          marginBottom: '8px',
+        }}
+      >
+        리뷰
+      </Typography>
+      <ReviewTextField
+        value={value}
+        onChange={onTextFieldChange}
+        placeholder="가게에 대한 리뷰를 남겨주세요! (최소 50자, 최대 300자)"
+        multiline
+      />
+    </ReviewCommentWrapper>
+  );
+}
+
+const ReviewCommentWrapper = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  marginBottom: '24px',
+});
+
+const ReviewTextField = styled(TextField)({
+  '& .MuiInputBase-root': {
+    padding: '16px',
+  },
+  '& fieldset': {
+    border: '1px solid #a4aaae',
+  },
+  '& textarea': {
+    minHeight: '136px',
+    maxHeight: '136px',
+  },
+});
