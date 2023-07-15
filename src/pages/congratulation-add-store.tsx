fa653ccc84cde660cbea05220bf008c074f964c2
Self-knowledge <qmfahs@gmail.com>
2023-07-16 02:29:55 +0900

    fix: rename export default function on congratulation-add-store page

--- src/pages/congratulation-add-store.tsx
+++ src/pages/congratulation-add-store.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/navigation';
 
 import { CongratulationCard } from '@/components/Misc/CongratulationCard';
 
-export default function CongratulationSignUp() {
+export default function CongratulationAddStore() {
   const router = useRouter();
 
   function handleButtonClick() {
