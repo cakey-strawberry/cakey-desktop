a7a5b46de87a545f4f273bff746d9e3db4ab7002
Self-knowledge <qmfahs@gmail.com>
2023-07-22 17:56:06 +0900

    fix: remove checkbox for no address from StoreAddressInput on add-store page

--- src/components/AddStore/StoreAddressInput/StoreAddressInput.tsx
+++ src/components/AddStore/StoreAddressInput/StoreAddressInput.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import { useController } from 'react-hook-form';
 import { styled, Box, Typography } from '@mui/material';
 
 import { Input } from '@/common/components/Input';
-import { Checkbox } from '@/common/components/Checkbox';
 
 import type { Control } from 'react-hook-form';
 import type { FormValues } from '@/pages/add-store';
@@ -13,30 +11,16 @@ type StoreAddressInputProps = {
 };
 
 export default function StoreAddressInput({ control }: StoreAddressInputProps) {
-  const [hasAddress, setHasAddress] = useState<boolean>(false);
-  const {
-    field: { onChange },
-  } = useController({
+  useController({
     control,
     name: 'storeAddress',
     rules: {
-      required: hasAddress ? false : {
+      required: {
         value: true,
         message: '',
       },
     },
   });
-
-  function handleAddressCheckboxToggle(
-      event: React.ChangeEvent<HTMLInputElement>,
-  ) {
-    const currentChecked = event.target.checked;
-    setHasAddress(currentChecked);
-
-    if (currentChecked) {
-      onChange('');
-    }
-  }
 
   return (
     <StoreAddressInputWrapper>
@@ -50,19 +34,7 @@ export default function StoreAddressInput({ control }: StoreAddressInputProps) {
         placeholder="가게 주소를 입력해주세요."
         fullWidth
         autoComplete="off"
-        disabled={hasAddress}
       />
-      <StoreAddressCheckboxWrapper>
-        <Checkbox
-          checked={hasAddress}
-          onChange={handleAddressCheckboxToggle}
-          sx={{
-            width: '24px',
-            height: '24px',
-          }}
-        />
-        <StoreAddressLabelText>주소가 없습니다.</StoreAddressLabelText>
-      </StoreAddressCheckboxWrapper>
     </StoreAddressInputWrapper>
   );
 }
@@ -102,24 +74,4 @@ const RequiredFieldIndicator = styled(Typography)({
   fontWeight: '500',
   lineHeight: '20px',
   letterSpacing: '0.1px',
-});
-
-const StoreAddressCheckboxWrapper = styled(Box)({
-  display: 'flex',
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-  gap: '8px',
-  width: '400px',
-  height: '40px',
-});
-
-const StoreAddressLabelText = styled(Typography)({
-  fontFamily: 'Pretendard',
-  fontWeight: '400',
-  fontSize: '14px',
-  lineHeight: 'normal',
-  display: 'flex',
-  alignItems: 'center',
-  letterSpacing: '0.25px',
-  color: '#6C757D',
 });
