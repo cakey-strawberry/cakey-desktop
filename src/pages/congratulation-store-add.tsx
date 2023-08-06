233ec5f4de03427a87ae628c632f2abedcc76316
Self-knowledge <qmfahs@gmail.com>
2023-08-06 16:13:40 +0900

    refactor: modify congratulation-store-add page to configure layout without CongratulationCard

--- src/pages/congratulation-store-add.tsx
+++ src/pages/congratulation-store-add.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { styled, Box } from '@mui/material';
 
-import { CongratulationCard } from '@/components/Misc/CongratulationCard';
+import { ContentHeader } from '@/common/components/ContentHeader';
+import { NextButton } from '@/components/Auth/NextButton';
+import CongratulationImage
+  from '@/common/assets/icons/congratulation-check.svg';
 
-export default function CongratulationStoreAdd() {
+export default function CongratulationSignUp() {
   const router = useRouter();
 
   function handleButtonClick() {
@@ -10,14 +15,36 @@ export default function CongratulationStoreAdd() {
   }
 
   return (
-    <CongratulationCard
-      title="스토어 등록을 완료하였습니다!"
-      subtitle="스토어 검토 후 등록이 완료됩니다."
-      buttonText="메인으로"
-      onButtonClick={handleButtonClick}
-      sx={{
-        marginTop: '60px',
-      }}
-    />
+    <PageWrapper>
+      <ContentHeader
+        title="스토어 등록을 완료하였습니다!"
+        subtitle="스토어 검토 후 등록이 완료됩니다."
+      />
+      <PageContent>
+        <Image
+          src={CongratulationImage}
+          alt="축하 이미지"
+          width={160}
+          height={160}
+        />
+        <NextButton type="button" text="메인으로" onClick={handleButtonClick} />
+      </PageContent>
+    </PageWrapper>
   );
 }
+
+export const PageWrapper = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '0px',
+  gap: '40px',
+  marginTop: '60px',
+});
+
+export const PageContent = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '40px',
+});
