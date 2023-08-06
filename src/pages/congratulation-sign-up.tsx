f6e64773d0a75b6d255622e0446a472eca421b78
Self-knowledge <qmfahs@gmail.com>
2023-08-06 16:12:57 +0900

    refactor: modify congratulation-sign-up page to configure layout without CongratulationCard

--- src/pages/congratulation-sign-up.tsx
+++ src/pages/congratulation-sign-up.tsx
@@ -1,6 +1,11 @@
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { styled, Box } from '@mui/material';
 
-import { CongratulationCard } from '@/components/Misc/CongratulationCard';
+import { ContentHeader } from '@/common/components/ContentHeader';
+import { NextButton } from '@/components/Auth/NextButton';
+import CongratulationImage
+  from '@/common/assets/icons/congratulation-check.svg';
 
 export default function CongratulationSignUp() {
   const router = useRouter();
@@ -10,14 +15,36 @@ export default function CongratulationSignUp() {
   }
 
   return (
-    <CongratulationCard
-      title="진심으로 환영합니다!"
-      subtitle="가입이 완료 되었어요."
-      buttonText="로그인하기"
-      onButtonClick={handleButtonClick}
-      sx={{
-        marginTop: '60px',
-      }}
-    />
+    <PageWrapper>
+      <ContentHeader
+        title="진심으로 환영합니다!"
+        subtitle="가입이 완료 되었어요."
+      />
+      <PageContent>
+        <Image
+          src={CongratulationImage}
+          alt="축하 이미지"
+          width={160}
+          height={160}
+        />
+        <NextButton type="button" text="로그인하기" onClick={handleButtonClick} />
+      </PageContent>
+    </PageWrapper>
   );
 }
+
+export const PageWrapper = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '0px',
+  gap: '40px',
+  marginTop: '60px',
+});
+
+export const PageContent = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '40px',
+});
