d86bd2f9efe21c8a3d75fb9f640196d95c932ba1
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2024-07-27 13:35:04 +0900

    feat: add Auth repository

--- src/common/repositories/auth/auth.ts
+++ src/common/repositories/auth/auth.ts
@@ -3,10 +3,17 @@ import { AuthEndpointSet } from '@/common/service/endpoint';
 
 import type { GoogleLoginResponse } from './types';
 
+type GoogleLoginRequestParams = {
+  code: string;
+};
+
 export class AuthRepository {
-  static async googleLogin() {
-    return api.get<GoogleLoginResponse>({
+  static async googleLogin({ code }: GoogleLoginRequestParams) {
+    return api.post<GoogleLoginResponse>({
       endpointSet: AuthEndpointSet.auth.googleLogin,
+      body: {
+        code,
+      },
     });
   }
 }
