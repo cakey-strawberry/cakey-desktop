8a0bf622d3e3f01ce943ef986f6843ea0357547f
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2024-07-30 20:56:16 +0900

    chore: add env

--- env.d.ts
+++ env.d.ts
@@ -2,5 +2,7 @@ namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_HOST_URL: string;
     NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID: string;
+    NEXT_PUBLIC_KAKAO_OAUTH_CLIENT_ID: string;
+    NEXT_PUBLIC_KAKAO_OAUTH_REDIRECT_URI: string;
   }
 }
